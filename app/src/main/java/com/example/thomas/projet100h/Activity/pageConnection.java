60e9045c5d1c86016721efb7ba99cd110f05b80b
Thomas <thomas.fontaine@hei.fr>
2017-02-07 18:58:33 +0100

    commit 07/02

--- app/src/main/java/com/example/thomas/projet100h/Activity/pageConnection.java
+++ app/src/main/java/com/example/thomas/projet100h/Activity/pageConnection.java
@@ -1,12 +1,11 @@
 package com.example.thomas.projet100h.Activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
-import com.example.thomas.projet100h.Activity.Acceuil;
 import com.example.thomas.projet100h.R;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -15,12 +14,27 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class pageConnection extends AppCompatActivity {
 
     private CallbackManager callbackManager;
     private Intent i;
     private LoginButton loginButton;
+    private static final String TAG_URL_USER ="http://localhost:8080/heisenbears/user/";
+    private String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,30 +45,32 @@ public class pageConnection extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.loginButton);
         loginButton.setReadPermissions("email");
-        Log.e("yo",callbackManager.toString());
 
 
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-
             @Override
             public void onSuccess(LoginResult loginResult) {
-                i.putExtra("id",loginResult.getAccessToken().getUserId());
-                startActivity(i);
+                Log.e("Yo","1");
+                Log.e("Yo",loginResult.toString());
+                Log.e("Yo",loginResult.getAccessToken().toString());
+
+                id = loginResult.getAccessToken().getUserId();
+                Log.e("Yo",id);
+
             }
 
             @Override
-            public void onCancel() {
-                Log.e("Yo","2");
+            public void onCancel() {   Log.e("Yo","cancel");
             }
 
             @Override
             public void onError(FacebookException exception) {
-                Log.e("Yo","3");
+                Log.e("Yo","error");
             }
         });
 
-
+        Connection();
     }
 
     @Override
@@ -67,6 +83,77 @@ public class pageConnection extends AppCompatActivity {
     public void onClick(View V){
         i.putExtra("id",0);
         startActivity(i);
+    }
+
+    public void Connection(){
+
+        class TheTask  extends AsyncTask<String, String, String> {
+
+            @Override
+            protected void onPreExecute() {
+                // TODO Auto-generated method stub
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                Log.e("Yo","3");
+                DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+                Log.e("Yo",TAG_URL_USER+"1");
+                String URL = TAG_URL_USER+"1";
+                HttpGet httpget = new HttpGet(URL);
+                // Depends on your web service
+                httpget.setHeader("Content-type", "application/json");
+                InputStream inputStream = null;
+                String result = null;
+
+
+                try {
+                    Log.e("Yo","4");
+                    HttpResponse response = httpclient.execute(httpget);
+                    Log.e("ae",response.getStatusLine().toString());
+                    HttpEntity entity = response.getEntity();
+                    inputStream = entity.getContent();
+                    // json is UTF-8 by default
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+                    Log.e("YP",sb.toString());
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        Log.e("YP",line);
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                } catch (Exception e) {
+                }
+                finally {
+                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                try {
+
+                    JSONObject jsonObj = new JSONObject(result);
+                    String  nom = jsonObj.getString("nom");
+                    String  prenom = jsonObj.getString("prenom");
+                    String idStatut = jsonObj.getString("idStatut");
+
+
+
+
+                }  catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+
+        new TheTask().execute(TAG_URL_USER+"1");
     }
 
 }
