28300e1606138c814b6de09d93fa1c01c9bb31a2
Thomas <thomas.fontaine@hei.fr>
2017-05-11 16:09:49 +0200

    regle

--- app/src/main/java/com/example/thomas/projet100h/Activity/jeu.java
+++ app/src/main/java/com/example/thomas/projet100h/Activity/jeu.java
@@ -73,11 +73,12 @@ public class jeu extends AppCompatActivity
             intent = new Intent(this, Actu.class);
             startActivity(intent);
         } else if (id == R.id.nav_jeu) {
-
+            intent = new Intent(this, jeu.class);
+            startActivity(intent);
         } else if (id == R.id.nav_equipe) {
-
+            intent = new Intent(this, roster.class);
+            startActivity(intent);
         } else if (id == R.id.nav_resCal) {
-
         } else if (id == R.id.nav_profil) {
             intent = new Intent(this, profil.class);
             startActivity(intent);
